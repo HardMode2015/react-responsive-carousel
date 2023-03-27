1041b64140878b286f147e9c6621c46fae238b0c
Foo <twistleech@gmail.com>
2023-03-27 08:30:35 +0200

    react 17

--- codesandbox/default/src/Carousel.js
+++ codesandbox/default/src/Carousel.js
@@ -8,15 +8,14 @@ const images = [
     'https://realstate.test/storage/properties/test2/3475035-e6787o.webp',
     'https://realstate.test/storage/properties/test2/3475035-e6787o.webp',
     'https://realstate.test/storage/properties/test2/3475035-e6787o.webp',
-    'https://realstate.test/storage/properties/test2/3475035-e6787o.webp',
-]
+    'https://realstate.test/storage/properties/test2/3475035-e6787o.webp'
+];
 
 export default () => (
     <Carousel autoPlay>
         {images.length ? images.map((image, i) => (
             <div key={i}>
             <img alt="" src={image} />
-            <p className="legend">Legend 1</p>
         </div>
         )): null}
     </Carousel>
